e913521aa8d383263c892973b8886db2ba281941
Nopu70 <nopu70@hotmail.com>
2017-07-12 15:22:37 +0800

    添加对房间号的支持

--- app/src/main/java/com/hehe/sharingilive/MyApplication.java
+++ app/src/main/java/com/hehe/sharingilive/MyApplication.java
@@ -3,7 +3,11 @@ package com.hehe.sharingilive;
 import android.app.Application;
 import android.content.Context;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
 import com.ucloud.ulive.UStreamingContext;
+
+import cn.bmob.v3.Bmob;
 
 /**
  * Created by tarena on 2017/7/11.
@@ -18,9 +22,6 @@ public class MyApplication extends Application {
         Bmob.initialize(this,"e8c8cf2d8f11e100ee821f5eb6ae6ced");
         EMOptions options = new EMOptions();
         EMClient.getInstance().init(this, options);
-
-
-
 
         context = getApplicationContext();
         UStreamingContext.init(context, "publish3-key");
