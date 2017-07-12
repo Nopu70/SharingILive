6519c752e2309672fcf703dc2bbfc3e34fcc3f0f
Nopu70 <nopu70@hotmail.com>
2017-07-12 15:20:25 +0800

    gradle add

--- app/src/main/java/com/hehe/sharingilive/MyApplication.java
+++ app/src/main/java/com/hehe/sharingilive/MyApplication.java
@@ -3,6 +3,8 @@ package com.hehe.sharingilive;
 import android.app.Application;
 import android.content.Context;
 
+import com.ucloud.ulive.UStreamingContext;
+
 /**
  * Created by tarena on 2017/7/11.
  */
@@ -14,5 +16,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        UStreamingContext.init(context, "publish3-key");
     }
 }
--- app/src/main/java/com/hehe/sharingilive/util/UserDialog.java
+++ app/src/main/java/com/hehe/sharingilive/util/UserDialog.java
@@ -2,6 +2,10 @@ package com.hehe.sharingilive.util;
 
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.hehe.sharingilive.R;
 
 /**
  * Created by tarena on 2017/7/12.
@@ -9,12 +13,27 @@ import android.support.v7.app.AlertDialog;
 
 public class UserDialog {
 
+    AlertDialog dialog;
+
     public static enum TYPE {
         USER, ADMIN
     }
 
     public UserDialog(TYPE type, Context context){
-        AlertDialog dialog = new AlertDialog.Builder(context)
-                .create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View view = View.inflate(context, R.layout.user_dialog_view, null);
+        builder.setView(view);
+        ImageView cls_btn = view.findViewById(R.id.cls_btn);
+        cls_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog = builder.create();
+    }
+
+    public void showDialog(){
+        dialog.show();
     }
 }
